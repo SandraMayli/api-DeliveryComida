634174d901515feb3280f08011a4b950a58d3342
Sandra Rodriguez <srodriguezg8@upao.edu.pe>
2023-04-23 03:57:38 -0500

    feat:  Crear controlador OrderController para manejar las solicitudes HTTP

---
+++ Delibery/Delibery/src/main/java/com/delibery/delibery/Controller/OrderController.java
@@ -0,0 +1,75 @@
+package com.delibery.delibery.Controller;
+
+import com.delibery.delibery.Clases.Order;
+import com.delibery.delibery.Services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        if (orders.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(orders);
+        }
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        Optional<Order> order = orderService.getOrderById(id);
+        if (order.isPresent()) {
+            return ResponseEntity.ok(order.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        orderService.createOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order order) {
+        Optional<Order> existingOrder = orderService.getOrderById(id);
+        if (existingOrder.isPresent()) {
+            order.setId(id);
+            orderService.updateOrder(order);
+            return ResponseEntity.ok(order);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        Optional<Order> order = orderService.getOrderById(id);
+        if (order.isPresent()) {
+            orderService.deleteOrder(order.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
